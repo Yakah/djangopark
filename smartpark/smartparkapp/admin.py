948e7fe4f317c821ba4fd229be1e87d06bf2eb75
Yakah <franckarani@gmail.com>
2024-09-09 05:36:33 +0300

    updated admin.py

--- smartpark/smartparkapp/admin.py
+++ smartpark/smartparkapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Video, parkingSlot, vehicle, Item
+from embed_video.admin import AdminVideoMixin
 
 # Register your models here.
+admin.site.register(Video)
+admin.site.register(parkingSlot)
+admin.site.register(vehicle)
+
+
+class MyModelAdmin(AdminVideoMixin, admin.ModelAdmin):
+    pass
+
+admin.site.register(Item, MyModelAdmin)
